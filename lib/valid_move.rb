ec9f6fca55a58e4d6ab1c8ad41dc536167f0ec79
Christopher Finch <christopher.h.finch@gmail.com>
2018-01-31 03:50:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,11 +8,10 @@ end
 
   # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-#def position_taken?(array_in, index_in)
-#  if array[indexs]==" "||array[indexs]==""||array[indexs]==nil
-#    true
-#  else array[indexs]=="X"||array[indexs]=="O"
-#    false
-#  end
-
-#end
+def position_taken?(array_in, index_in)
+  if array[indexs]==" "||array[indexs]==""||array[indexs]==nil
+    true
+  else array[indexs]=="X"||array[indexs]=="O"
+    false
+  end
+end
